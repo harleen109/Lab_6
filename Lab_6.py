db41bfdd1c56db8a067899d40cfaf4b9e4bc66c2
harleen109 <harleen109@mail.fresnostate.edu>
2020-12-15 23:41:45 -0800

    Finished pseudo-code

--- Lab_6.py
+++ Lab_6.py
@@ -1,17 +1,22 @@
 
-
-
-
 """
-To start, we will generate a random intefer between 1 and 20, and
-based on the result of the random number, we check to see if it fails under certain range
-if the number is greater than 15, then the result will be "Cherries"
-otherwise if the number is > 10, then the result will be "Orange"
-otherwise if the number is > 5, then the result will be "Plum"
-otherwise if the number is > 2, then the resul twill be "Melon"
-otherwise if the number is > 1, then the result will be "Bell"
-if the number is not any of the above, then the result will be "Bar"
+import random
+num = generate random number
 
-we iterate over using a loop three times and print the results to the user. As an example "Plum Cherries"
+if num is greater than 15,
+  then the result will be "Cherries"
+otherwise if num is > 10,
+  then the result will be "Orange"
+otherwise if num is > 5,
+  then the result will be "Plum"
+otherwise if num is > 2, 
+  then the result will be "Melon"
+otherwise if num is > 1, 
+  then the result will be "Bell"
+otherwise
+  the result will be "Bar"
+
+loop three times
+  print the output (fruit) to the user
 
 """
